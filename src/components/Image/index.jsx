1c3c2ca52092969efdc976e7ed73b88c29006947
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2021-07-11 21:40:50 +0200

    increase image quality

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Image as CloudinaryImage } from 'cloudinary-react';
+import { Image as CloudinaryImage, Transformation } from 'cloudinary-react';
 
 export const Image = ({ imgId, width = "300" }) => {
     const cloudName = process.env.REACT_APP_CLOUDINARY_SPACE_ID;
-    return (<CloudinaryImage cloudName={cloudName} publicId={imgId} width={width} height={width} crop="fit" />);
+    return (
+        <CloudinaryImage cloudName={cloudName} publicId={imgId} width={width} height={width} crop="fit">
+            <Transformation quality="100" />
+        </CloudinaryImage>
+    );
 };
