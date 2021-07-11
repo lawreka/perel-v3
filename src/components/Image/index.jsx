3de3e29d25a63c365250c74316b5ea1f8f636024
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2021-07-11 21:44:05 +0200

    revert image optimization

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { Image as CloudinaryImage, Transformation } from 'cloudinary-react';
+import { Image as CloudinaryImage } from 'cloudinary-react';
 
 export const Image = ({ imgId, width = "300" }) => {
     const cloudName = process.env.REACT_APP_CLOUDINARY_SPACE_ID;
-    return (
-        <CloudinaryImage cloudName={cloudName} publicId={imgId} width={width} height={width} crop="fit">
-            <Transformation quality="100" />
-        </CloudinaryImage>
-    );
+    return (<CloudinaryImage cloudName={cloudName} publicId={imgId} width={width} height={width} crop="fit" />);
 };
