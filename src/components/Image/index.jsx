3bf78fa41218dca03acba48c7731608c16bd7ba8
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2021-07-11 22:13:53 +0200

    add lazy loading

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -3,5 +3,5 @@ import { Image as CloudinaryImage } from 'cloudinary-react';
 
 export const Image = ({ imgId, width = "300" }) => {
     const cloudName = process.env.REACT_APP_CLOUDINARY_SPACE_ID;
-    return (<CloudinaryImage cloudName={cloudName} publicId={imgId} width={width} height={width} crop="fit" />);
+    return (<CloudinaryImage cloudName={cloudName} publicId={imgId} width={width} height={width} loading="lazy" crop="fit" />);
 };
